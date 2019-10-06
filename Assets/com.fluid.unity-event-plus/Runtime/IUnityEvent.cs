897fb744b85337d694d8a95167973dfbab7b8c2c
Ash Blue <ash@blueashes.com>
2019-10-06 16:00:20 -0600

    fix: added missing clear event method

--- Assets/com.fluid.unity-event-plus/Runtime/IUnityEvent.cs
+++ Assets/com.fluid.unity-event-plus/Runtime/IUnityEvent.cs
@@ -5,23 +5,27 @@ namespace CleverCrow.Fluid.Utilities.UnityEvents {
         void AddListener (UnityAction call);
         void RemoveListener (UnityAction call);
         void Invoke ();
+        void RemoveAllListeners ();
     }
 
     public interface IUnityEvent<T1> {
         void AddListener (UnityAction<T1> call);
         void RemoveListener (UnityAction<T1> call);
         void Invoke (T1 arg1);
+        void RemoveAllListeners ();
     }
 
     public interface IUnityEvent<T1, T2> {
         void AddListener (UnityAction<T1, T2> call);
         void RemoveListener (UnityAction<T1, T2> call);
         void Invoke (T1 arg1, T2 arg2);
+        void RemoveAllListeners ();
     }
 
     public interface IUnityEvent<T1, T2, T3> {
         void AddListener (UnityAction<T1, T2, T3> call);
         void RemoveListener (UnityAction<T1, T2, T3> call);
         void Invoke (T1 arg1, T2 arg2, T3 arg3);
+        void RemoveAllListeners ();
     }
 }
